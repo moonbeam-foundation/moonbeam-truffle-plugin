410050f02b0afc0b96c99d1e32ca0905697cbee3
eshaben <eshaben@icloud.com>
2021-03-30 11:59:54 -0600

    clean up callback

--- commands/start_node.js
+++ commands/start_node.js
@@ -11,41 +11,28 @@ const isMacOS = () => {
    }
 }
 
+const callback = (error, stdout, stderr) => {
+   if (error) {
+      if (error.message.includes('permission denied')) {
+         console.log(`Connect: permission denied. Permission issues, try again with sudo`);
+      } else {
+         console.log(`Error: ${error.message}`);
+      }
+      return;
+   }
+   if (stderr) {
+      console.log(`Error: ${stderr}`);
+      return;
+   }
+   console.log(`Node has started - Endpoints: HTTP http://127.0.0.1:9933  WS ws://127.0.0.1:9944 - Container ID ${stdout.substr(0, 12)} \n`);
+};
+
 const start = async (version) => {
    // Start Node
-   const isMac = isMacOS();
-   if (isMac){
-      exec(`docker run --rm -d --name moonbeam_standalone -p 9944:9944 -p 9933:9933 purestake/moonbeam:${version} --dev --ws-external --rpc-external`, (error, stdout, stderr) => {
-         if (error) {
-            if (error.message.includes('permission denied')) {
-               console.log(`Connect: permission denied. Permission issues, try again with sudo`);
-            } else {
-               console.log(`Error: ${error.message}`);
-            }
-            return;
-         }
-         if (stderr) {
-            console.log(`Error: ${stderr}`);
-            return;
-         }
-         console.log(`Node has started - Endpoints: HTTP http://127.0.0.1:9933  WS ws://127.0.0.1:9944 - Container ID ${stdout.substr(0, 12)} \n`);
-      });
+   if (isMacOS()){
+      exec(`docker run --rm -d --name moonbeam_standalone -p 9944:9944 -p 9933:9933 purestake/moonbeam:${version} --dev --ws-external --rpc-external`, callback);
    } else {
-      exec(`docker run --rm -d --name moonbeam_standalone --network host purestake/moonbeam:${version} --dev`, (error, stdout, stderr) => {
-         if (error) {
-            if (error.message.includes('permission denied')) {
-               console.log(`Connect: permission denied. Permission issues, try again with sudo`);
-            } else {
-               console.log(`Error: ${error.message}`);
-            }
-            return;
-         }
-         if (stderr) {
-            console.log(`Error: ${stderr}`);
-            return;
-         }
-         console.log(`Node has started - Endpoints: HTTP http://127.0.0.1:9933  WS ws://127.0.0.1:9944 - Container ID ${stdout.substr(0, 12)} \n`);
-      });
+      exec(`docker run --rm -d --name moonbeam_standalone --network host purestake/moonbeam:${version} --dev`, callback);
    }
 };
 
